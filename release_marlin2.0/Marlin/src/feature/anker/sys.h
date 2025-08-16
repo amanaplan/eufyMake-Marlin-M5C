0cea75f135e6b9159206e637e6ee33bf623cb607
Christian Stück <christianstuck@gmail.com>
2025-08-16 17:09:23 -0400

    Translated comments in sys.h to English

--- release_marlin2.0/Marlin/src/feature/anker/sys.h
+++ release_marlin2.0/Marlin/src/feature/anker/sys.h
@@ -2,13 +2,15 @@
 #define __SYS_H	 
 #include "stm32f4xx.h" 
  
-//位带操作,实现51类似的GPIO控制功能
-//具体实现思想,参考<<CM3权威指南>>第五章(87页~92页).M4同M3类似,只是寄存器地址变了.
-//IO口操作宏定义
+// Bit-band operations, implementing GPIO control functions similar to the 51.
+// For detailed implementation ideas, refer to Chapter 5 (pages 87-92) of the
+//   <<CM3 Definitive Guide>>. The M4 is similar to the M3, except that the
+//   register addresses are different.
+// IO Port Operation Macro Definitions
 #define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
 #define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
 #define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
-//IO口地址映射
+// IO Port Address Mapping
 #define GPIOA_ODR_Addr    (GPIOA_BASE+20) //0x40020014
 #define GPIOB_ODR_Addr    (GPIOB_BASE+20) //0x40020414 
 #define GPIOC_ODR_Addr    (GPIOC_BASE+20) //0x40020814 
@@ -28,37 +30,37 @@
 #define GPIOG_IDR_Addr    (GPIOG_BASE+16) //0x40021810 
 #define GPIOH_IDR_Addr    (GPIOH_BASE+16) //0x40021C10 
 #define GPIOI_IDR_Addr    (GPIOI_BASE+16) //0x40022010 
- 
-//IO口操作,只对单一的IO口!
-//确保n的值小于16!
-#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出 
-#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入 
 
-#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出 
-#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入 
+// IO Port Operation, only for a single IO port
+// Make sure the value of n is less than 16
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  // Output
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  // Input
 
-#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //输出 
-#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //输入 
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  // Output
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  // Input 
 
-#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //输出 
-#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //输入 
+#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  // Output
+#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  // Input
 
-#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //输出 
-#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //输入
+#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  // Output
+#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  // Input
 
-#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //输出 
-#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //输入
+#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  // Output
+#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  // Input
 
-#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //输出 
-#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //输入
+#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  // Output
+#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  // Input
 
-#define PHout(n)   BIT_ADDR(GPIOH_ODR_Addr,n)  //输出 
-#define PHin(n)    BIT_ADDR(GPIOH_IDR_Addr,n)  //输入
+#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  // Output 
+#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  // Input
 
-#define PIout(n)   BIT_ADDR(GPIOI_ODR_Addr,n)  //输出 
-#define PIin(n)    BIT_ADDR(GPIOI_IDR_Addr,n)  //输入
+#define PHout(n)   BIT_ADDR(GPIOH_ODR_Addr,n)  // Output 
+#define PHin(n)    BIT_ADDR(GPIOH_IDR_Addr,n)  // Input
+
+#define PIout(n)   BIT_ADDR(GPIOI_ODR_Addr,n)  // Output 
+#define PIin(n)    BIT_ADDR(GPIOI_IDR_Addr,n)  // Input
 ////////////////////////////////////////////////////////////////////////////////// 
-//Ex_NVIC_Config专用定义
+//Ex_NVIC_Config Dedicated Definition
 #define GPIO_A 				0
 #define GPIO_B 				1
 #define GPIO_C				2
@@ -69,29 +71,29 @@
 #define GPIO_H 				7 
 #define GPIO_I 				8 
 
-#define FTIR   				1  		//下降沿触发
-#define RTIR   				2  		//上升沿触发
+#define FTIR   				1  		// Trigger on Falling Edge
+#define RTIR   				2  		// Trigger on Rising Edge
 
-//GPIO设置专用宏定义
-#define GPIO_MODE_IN    	0		//普通输入模式
-#define GPIO_MODE_OUT		1		//普通输出模式
-#define GPIO_MODE_AF		2		//AF功能模式
-#define GPIO_MODE_AIN		3		//模拟输入模式
+// GPIO Setting Dedicated Macro Definition
+#define GPIO_MODE_IN    	0		// Normal Input Mode
+#define GPIO_MODE_OUT		1		// Normal Output Mode
+#define GPIO_MODE_AF		2		// Alternate Function Mode
+#define GPIO_MODE_AIN		3		// Analog Input Mode
 
-#define GPIO_SPEED_2M		0		//GPIO速度2Mhz
-#define GPIO_SPEED_25M		1		//GPIO速度25Mhz
-#define GPIO_SPEED_50M		2		//GPIO速度50Mhz
-#define GPIO_SPEED_100M		3		//GPIO速度100Mhz
+#define GPIO_SPEED_2M		0		// GPIO Speed 2Mhz
+#define GPIO_SPEED_25M		1		// GPIO Speed 25Mhz
+#define GPIO_SPEED_50M		2		// GPIO Speed 50Mhz
+#define GPIO_SPEED_100M		3		// GPIO Speed 100Mhz
 
-#define GPIO_PUPD_NONE		0		//不带上下拉
-#define GPIO_PUPD_PU		1		//上拉
-#define GPIO_PUPD_PD		2		//下拉
-#define GPIO_PUPD_RES		3		//保留 
+#define GPIO_PUPD_NONE		0		// No Pull-Up or Pull-Down
+#define GPIO_PUPD_PU		1		// Pull-Up
+#define GPIO_PUPD_PD		2		// Pull-Down
+#define GPIO_PUPD_RES		3		// Reserved 
 
-#define GPIO_OTYPE_PP		0		//推挽输出
-#define GPIO_OTYPE_OD		1		//开漏输出 
+#define GPIO_OTYPE_PP		0		// Push-Pull Output
+#define GPIO_OTYPE_OD		1		// Open-Drain Output
 
-//GPIO引脚编号定义
+// GPIO PIN Number Definition
 #define PIN0				1<<0
 #define PIN1				1<<1
 #define PIN2				1<<2
@@ -113,18 +115,18 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 ////////////////////////////////////////////////////////////////////////////////// 
-u8 Sys_Clock_Set(u32 plln,u32 pllm,u32 pllp,u32 pllq);		//系统时钟设置
-void Stm32_Clock_Init(u32 plln,u32 pllm,u32 pllp,u32 pllq); //时钟初始化  
-void Sys_Soft_Reset(void);      							//系统软复位
-void Sys_Standby(void);         							//待机模式 	
-void MY_NVIC_SetVectorTable(u32 NVIC_VectTab, u32 Offset);	//设置偏移地址
-void MY_NVIC_PriorityGroupConfig(u8 NVIC_Group);			//设置NVIC分组
-void MY_NVIC_Init(u8 NVIC_PreemptionPriority,u8 NVIC_SubPriority,u8 NVIC_Channel,u8 NVIC_Group);//设置中断
-void Ex_NVIC_Config(u8 GPIOx,u8 BITx,u8 TRIM);				//外部中断配置函数(只对GPIOA~I)
-void GPIO_AF_Set(GPIO_TypeDef* GPIOx,u8 BITx,u8 AFx);		//GPIO复用功能设置
-void GPIO_Set(GPIO_TypeDef* GPIOx,u32 BITx,u32 MODE,u32 OTYPE,u32 OSPEED,u32 PUPD);//GPIO设置函数  
-//以下为汇编函数
-void WFI_SET(void);		//执行WFI指令
-void INTX_DISABLE(void);//关闭所有中断
-void INTX_ENABLE(void);	//开启所有中断
+u8 Sys_Clock_Set(u32 plln,u32 pllm,u32 pllp,u32 pllq);		// System Clock Settings
+void Stm32_Clock_Init(u32 plln,u32 pllm,u32 pllp,u32 pllq); // Clock Initialization 
+void Sys_Soft_Reset(void);      							// System Soft Reset
+void Sys_Standby(void);         							// System Standby Mode	
+void MY_NVIC_SetVectorTable(u32 NVIC_VectTab, u32 Offset);	// Set Offset Address
+void MY_NVIC_PriorityGroupConfig(u8 NVIC_Group);			// Set NVIC Group
+void MY_NVIC_Init(u8 NVIC_PreemptionPriority,u8 NVIC_SubPriority,u8 NVIC_Channel,u8 NVIC_Group);// Setup interrupts
+void Ex_NVIC_Config(u8 GPIOx,u8 BITx,u8 TRIM);				// External interrupt configuration function (GPIOA to I only)
+void GPIO_AF_Set(GPIO_TypeDef* GPIOx,u8 BITx,u8 AFx);		// GPIO Multiplexing Function Settings
+void GPIO_Set(GPIO_TypeDef* GPIOx,u32 BITx,u32 MODE,u32 OTYPE,u32 OSPEED,u32 PUPD); // GPIO Setup Function
+// The following is the assembly code
+void WFI_SET(void);		// Execute WFI Instruction
+void INTX_DISABLE(void); // Disable All Interrupts
+void INTX_ENABLE(void);	// Enable All Interrupts
 #endif
